745e1265ce0cb138ee31dfca447d422b85c51028
Max <wundel.uebr@yandex.ru>
2025-04-09 17:50:14 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ fn main() {
         .read_line(&mut input)
         .expect("Не удалось прочитать строку");
 
-    let begin_time = time::Instant::now();
     let n = input
         .trim()
         .parse::<usize>()
@@ -27,13 +26,14 @@ fn main() {
         exit(1);
     }
 
+    let begin_time = time::Instant::now();
     let mut analyze = Analyze::new(countries, true);
     let d = analyze.cluster_n_times(n);
+    let end_time = time::Instant::now();
 
     analyze.print();
 
     println!("Расстояние между последними объединенными кластерами: {d}");
-    let end_time = time::Instant::now();
     println!(
         "Время выполнения: {} секунд!\nА питон так сможет?? ХАХАХАХАХА",
         end_time.duration_since(begin_time).as_secs_f64()
