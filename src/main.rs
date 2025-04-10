07d0fdea8fe62948d6edd56d9afa858a33aeab12
maxpermyakov <wundel.uber@yandex.ru>
2025-04-10 12:12:54 +0300

    supercommit

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,6 @@ fn main() {
         .read_line(&mut input)
         .expect("Не удалось прочитать строку");
 
-    let begin_time = time::Instant::now();
     let n = input.trim()
         .parse::<usize>()
         .expect("Пожалуйста введите число");
@@ -23,12 +22,14 @@ fn main() {
         exit(1);
     }
 
+    let begin_time = time::Instant::now();
     let mut analyze = Analyze::new(countries, true);
     let d = analyze.cluster_n_times(n);
+    let end_time = time::Instant::now();
+
 
     analyze.print();
 
     println!("Расстояние между последними объединенными кластерами: {d}");
-    let end_time = time::Instant::now();
     println!("Время выполнения: {} секунд!\nА питон так сможет?? ХАХАХАХАХА", end_time.duration_since(begin_time).as_secs_f64());
 }
